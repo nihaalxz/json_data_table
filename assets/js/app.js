643ad8b93815c06e5eb278bdb10cbd2d07f6c266
nihaalxz <nihaal1010@gmail.com>
2025-01-20 16:14:08 +0530

    minor changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,29 @@
 $(document).ready(() => {
     $.getJSON("assets/json/MOCK_DATA.json",
         function (data) {
-             person = '';
+            let person = '<tbody>';
             $.each(data, function (key,value) {
                 person += '<tr>';
                 person += '<td><img src="' + value.profile + '"></td>'
-                person += '<td>' + value.first_name + "&nbsp" + value.last_name + '</td>';
+                person += '<td>' + value.first_name + " " + value.last_name + '</td>';
                 person += '<td>' + value.gender+ '</td>';
                 person += '<td>' + value.email + '</td>';
                 person += '</tr>';
             });
-            $("table").append(person);
+            person += '</tbody>'
+            $("#person_table").append(person);
+            $('#person_table').DataTable();
         });
 
-        $("#excel").click(()=>{
-            $.getJSON('assets/json/MOCK_DATA.json', function(jsonData) {
-                const worksheet = XLSX.utils.json_to_sheet(jsonData);
-                const workbook = XLSX.utils.book_new();
-                XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-                XLSX.writeFile(workbook, "data.xlsx");
-            })
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                console.error("Error fetching or processing the JSON data: ", textStatus, errorThrown);
-            });
-        
+    $("#excel").click(() => {
+        $.getJSON('assets/json/MOCK_DATA.json', function(jsonData) {
+            const worksheet = XLSX.utils.json_to_sheet(jsonData);
+            const workbook = XLSX.utils.book_new();
+            XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+            XLSX.writeFile(workbook, "data.xlsx");
         })
-        
-
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            console.error("Error fetching or processing the JSON data: ", textStatus, errorThrown);
+        });
+    });
 });
-
